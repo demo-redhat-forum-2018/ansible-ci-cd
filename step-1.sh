1c204121482b1c2d0551bc94387f9cfb07978803
Maxime Clerix <mclerix@redhat.com>
2018-10-10 15:25:39 +0200

    Add demo script

---
+++ step-1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+oc login https://openshift.openhybridcloud.io:8443 -u demo -p redhat123
+
+git checkout step-1
+
+ansible-playbook apply.yml -i inventory/ -e target=bootstrap
+ansible-playbook apply.yml -i inventory/ -e target=tools
+ansible-playbook apply.yml -i inventory/ -e target=apps
+
+oc tag mono-ci-cd/coolstore:latest mono-ci-cd/coolstore:promoteToTest
+oc tag mono-ci-cd/coolstore:latest mono-ci-cd/coolstore:promoteToProd
+
+oc rollout latest coolstore -n mono-test
+oc rollout latest coolstore-blue -n mono-prod
+oc rollout latest coolstore-green -n mono-prod
+
+git checkout master
---
+++ step-2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+oc login https://openshift.openhybridcloud.io:8443 -u demo -p redhat123
+oc delete project mono-dev mono-test
+
+git checkout step-2-ovh
+ansible-playbook apply.yml -i inventory/ -e target=tools
+ansible-playbook apply.yml -i inventory/ -e target=apps
+
+oc login  https://ocp-rh-forum.northeurope.cloudapp.azure.com:8443 -u demo -p redhat123
+git checkout step-2-azure
+ansible-playbook apply.yml -i inventory/ -e target=bootstrap
+ansible-playbook apply.yml -i inventory/ -e target=apps
+
+git checkout step-master
